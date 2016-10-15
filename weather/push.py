40823ce23706ab140c75015114acd1744172b788
itisshreyas <shreyas.satardekar3@gmail.com>
2016-10-15 16:39:53 +0530

    Added the firstpage ui

--- weather/push.py
+++ weather/push.py
@@ -1,15 +1,4 @@
 from firebase import firebase
-from __future__ import with_statement
-from google.appengine.api import files
-import cloudstorage as gcs
-import main
-import webapp2
-from PIL import Image
-import base64
-import cStringIO
-import PIL.Image
-from google.appengine.ext import blobstore
-from google.appengine.ext.webapp import blobstore_handlers
 
 db = firebase.FirebaseApplication('https://agrilife-7cbc7.firebaseio.com/',None)
 
@@ -24,14 +13,10 @@ def pushdata(data):
 		wind = s["desc"]["wind"]
 		foot = s["foot"]
 		head = s["head"]
-		read_file(imageurl)
 		weatherstore = {"foot": foot, "head": head, "humidity": humidity, "image_url": imageurl, "overview": overview, "pressure": pressure, "wind": wind}
 		result = db.post('/weather_data', weatherstore)
+	
 
-def read_file(filename):
-    with open(filename, 'rb') as f:
-        photo = f.read()
-    return photo
 
 
 
