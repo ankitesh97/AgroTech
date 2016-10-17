b786a4ea94717b7504dbc88435a32e1a0dcad046
Ankitesh Gupta <ankiteshguptas@gmail.com>
2016-10-18 00:38:16 +0530

    printed the prediction

--- analytics/predict/scilearn.py
+++ analytics/predict/scilearn.py
@@ -5,6 +5,8 @@ import numpy as np
 
 db = firebase.FirebaseApplication('https://agrilife-7cbc7.firebaseio.com/',None)
 result = db.get('/dataset',None)
+crops_list = db.get('/crops',None)
+crops_list = crops_list.values()
 result = result.values()
 temp = [result[0]['N'],result[0]['P'],result[0]['K'],result[0]['water']]
 X = np.matrix(temp) #dataset
@@ -18,4 +20,8 @@ for i in range(1,len(result)):
 
 clf = svm.SVC(gamma=0.001, C=100)
 clf.fit(X,y)
-print clf.predict([20,20,18,15])
+prediction =  clf.predict([20,20,18,15])
+
+hand = filter(lambda x: x['id'] == prediction[0],crops_list).pop()
+
+print "predicted crops that can be grown is " + hand['name']
