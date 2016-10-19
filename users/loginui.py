7b744088b440b71a15e04b839e6a78dbaf9e6485
iamcodeblooded <anmol_yash@yahoo.co.in>
2016-10-19 21:38:35 +0530

    login ui updated

--- users/loginui.py
+++ users/loginui.py
@@ -2,12 +2,11 @@
 
 # Form implementation generated from reading ui file 'login.ui'
 #
-# Created: Wed Oct 19 20:22:29 2016
+# Created: Wed Oct 19 21:35:26 2016
 #      by: PyQt4 UI code generator 4.10.4
 #
 # WARNING! All changes made in this file will be lost!
 
-import sys
 from PyQt4 import QtCore, QtGui
 
 try:
@@ -18,64 +17,53 @@ except AttributeError:
 
 try:
     _encoding = QtGui.QApplication.UnicodeUTF8
-
     def _translate(context, text, disambig):
         return QtGui.QApplication.translate(context, text, disambig, _encoding)
 except AttributeError:
     def _translate(context, text, disambig):
         return QtGui.QApplication.translate(context, text, disambig)
 
-
-class Ui_Dialog(QtGui.QMainWindow, QtGui.QWidget):
-    def __init__(self):
-        QtGui.QWidget.__init__(self)
-        self.setupUi(self)
-
+class Ui_Dialog(object):
     def setupUi(self, Dialog):
         Dialog.setObjectName(_fromUtf8("Dialog"))
         Dialog.resize(398, 300)
         Dialog.setAutoFillBackground(True)
-        self.pushButton = QtGui.QPushButton(Dialog)
-        self.pushButton.setGeometry(QtCore.QRect(150, 210, 96, 26))
-        self.pushButton.setObjectName(_fromUtf8("pushButton"))
-        self.plainTextEdit = QtGui.QPlainTextEdit(Dialog)
-        self.plainTextEdit.setGeometry(QtCore.QRect(140, 60, 171, 31))
-        self.plainTextEdit.setObjectName(_fromUtf8("plainTextEdit"))
-        self.plainTextEdit_2 = QtGui.QPlainTextEdit(Dialog)
-        self.plainTextEdit_2.setGeometry(QtCore.QRect(140, 120, 171, 31))
-        self.plainTextEdit_2.setObjectName(_fromUtf8("plainTextEdit_2"))
-        self.label = QtGui.QLabel(Dialog)
-        self.label.setGeometry(QtCore.QRect(30, 60, 101, 31))
+        self.login = QtGui.QPushButton(Dialog)
+        self.login.setGeometry(QtCore.QRect(150, 210, 96, 26))
+        self.login.setObjectName(_fromUtf8("login"))
+        self.unametextbox = QtGui.QPlainTextEdit(Dialog)
+        self.unametextbox.setGeometry(QtCore.QRect(140, 60, 171, 31))
+        self.unametextbox.setObjectName(_fromUtf8("unametextbox"))
+        self.passtextbox = QtGui.QPlainTextEdit(Dialog)
+        self.passtextbox.setGeometry(QtCore.QRect(140, 120, 171, 31))
+        self.passtextbox.setObjectName(_fromUtf8("passtextbox"))
+        self.usernamelabel = QtGui.QLabel(Dialog)
+        self.usernamelabel.setGeometry(QtCore.QRect(30, 60, 101, 31))
         font = QtGui.QFont()
         font.setFamily(_fromUtf8("Georgia"))
         font.setPointSize(12)
         font.setBold(True)
         font.setWeight(75)
-        self.label.setFont(font)
-        self.label.setTextFormat(QtCore.Qt.RichText)
-        self.label.setObjectName(_fromUtf8("label"))
-        self.label_2 = QtGui.QLabel(Dialog)
-        self.label_2.setGeometry(QtCore.QRect(30, 130, 91, 17))
+        self.usernamelabel.setFont(font)
+        self.usernamelabel.setTextFormat(QtCore.Qt.RichText)
+        self.usernamelabel.setObjectName(_fromUtf8("usernamelabel"))
+        self.passwordlabel = QtGui.QLabel(Dialog)
+        self.passwordlabel.setGeometry(QtCore.QRect(30, 130, 91, 17))
         font = QtGui.QFont()
         font.setFamily(_fromUtf8("Georgia"))
         font.setPointSize(12)
         font.setBold(True)
         font.setItalic(False)
         font.setWeight(75)
-        self.label_2.setFont(font)
-        self.label_2.setObjectName(_fromUtf8("label_2"))
+        self.passwordlabel.setFont(font)
+        self.passwordlabel.setObjectName(_fromUtf8("passwordlabel"))
 
         self.retranslateUi(Dialog)
         QtCore.QMetaObject.connectSlotsByName(Dialog)
 
     def retranslateUi(self, Dialog):
         Dialog.setWindowTitle(_translate("Dialog", "Dialog", None))
-        self.pushButton.setText(_translate("Dialog", "Login", None))
-        self.label.setText(_translate("Dialog", "Username", None))
-        self.label_2.setText(_translate("Dialog", "Password", None))
+        self.login.setText(_translate("Dialog", "Login", None))
+        self.usernamelabel.setText(_translate("Dialog", "Username", None))
+        self.passwordlabel.setText(_translate("Dialog", "Password", None))
 
-if __name__ == '__main__':
-    app = QtGui.QApplication(sys.argv)
-    ex = Ui_Dialog()
-    ex.show()
-    sys.exit(app.exec_())
